52ef033524a4e98719bc05aee3b3f4fa0fe6668e
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-09-22 04:00:53 +0530

    com.ravan.maya

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -73,7 +73,7 @@ class DefaultFirebaseOptions {
     messagingSenderId: '210611802225',
     projectId: 'maya-app-cymake',
     storageBucket: 'maya-app-cymake.firebasestorage.app',
-    iosBundleId: 'com.com.ravan.Mayampany.myFlutterApp',
+    iosBundleId: 'com.ravan.maya',
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
